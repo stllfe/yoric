6c7157ee2399cf385eb8624a2891137c9e72bf9f
ilya <ilyastalk@bk.ru>
2023-04-19 00:02:46 +0500

    [#1][#4] fix evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 import re
-from src.model import yoModel
+from src.model import YoModel
 from tqdm import tqdm
 from typing import List, Tuple
 from src.utils import WORDS_REGEX
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from sklearn.metrics import accuracy_score, precision_score, recall_score, \
     f1_score, roc_auc_score
 
@@ -20,11 +20,11 @@ class EvaluateResult:
     auroc: float
 
     def __str__(self):
-        return "\n".join(["{}: {}".format(m, v) for m, v in self.__dict__.items()])
+        return "\n".join([f"{m}: {v}" for m, v in asdict(self).items()])
 
 
-def get_substrings(text: str) -> List[List[int]]:
-    """Function for get ye(yo) words indexes"""
+def get_substrings(text: str) -> List[Tuple[int]]:
+    """Extracts ye (yo) word indexes from text"""
     res = []
     for re_match in re.finditer(WORDS_REGEX, text):
         if re.search(r'[ЕЁеё]', re_match.group()) is not None:
@@ -32,8 +32,8 @@ def get_substrings(text: str) -> List[List[int]]:
     return res
 
 
-def substrings2onehot(text:str, positions: List[Tuple[int, int]]) -> np.ndarray[bool]:
-    """Transform substring positions to one hot vector"""
+def substrings2onehot(text: str, positions: List[Tuple[int, int]]) -> np.ndarray[bool]:
+    """Transforms substring positions to one hot vector"""
     words_pos = get_substrings(text)
     one_hot_index = np.zeros(len(words_pos), dtype=bool)
     for i, pos in enumerate(positions):
@@ -43,10 +43,10 @@ def substrings2onehot(text:str, positions: List[Tuple[int, int]]) -> np.ndarray[
     return one_hot_index
 
 
-def evaluate_model(model: yoModel, data: pd.DataFrame, verbose: bool=False) -> EvaluateResult:
+def evaluate_model(model: YoModel, data: pd.DataFrame, verbose: bool = False) -> EvaluateResult:
     X = data['ye_text'].to_list()
     y_true = data["yo_words"].to_list()
-    y_pred = model.predict(X, verbose)
+    y_pred = model.predict(X, verbose=verbose)
     
     y_true_onehot = []
     y_pred_onehot =[]
