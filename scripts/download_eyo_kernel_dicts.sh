ceca7db6661e2ad8ea570c2163a91b827e255021
Oleg Pavlovich <pavlovi4.o@gmail.com>
2023-11-21 13:54:03 +0300

    update: fix eyo dictionaries versions, reproduce again, update dvc config and docs

--- scripts/download_eyo_kernel_dicts.sh
+++ scripts/download_eyo_kernel_dicts.sh
@@ -3,5 +3,5 @@
 set -e
 
 mkdir -p data;
-curl -Lo data/not-safe.txt https://raw.githubusercontent.com/e2yo/eyo-kernel/master/dict_src/not_safe.txt
-curl -Lo data/safe.txt https://raw.githubusercontent.com/e2yo/eyo-kernel/master/dict_src/safe.txt
+curl -Lo data/not-safe.txt https://raw.githubusercontent.com/e2yo/eyo-kernel/b7ee2c50268c0e0bfcfa5d36fe9da613485ee43d/dictionary/not_safe.txt
+curl -Lo data/safe.txt https://raw.githubusercontent.com/e2yo/eyo-kernel/b7ee2c50268c0e0bfcfa5d36fe9da613485ee43d/dictionary/safe.txt
